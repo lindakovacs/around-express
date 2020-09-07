4918d377ba2b1b00faa6f505b4fcbdff94478321
Linda Kovacs <lindaprovenza@yahoo.com>
2020-09-07 15:22:18 -0400

    Create initial Backend server serving the static files from previous project

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cards = require('./routes/cards');
+const users = require('./routes/users');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/cards', cards);
+app.use('/users', users);
+
+app.get('*', (req, res) => {
+  res.status(404);
+  res.send({ message: 'Requested resource not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
